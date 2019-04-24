61803ae544972efa9a8ad6ee226467d0a29a29b0
sydneywait <sydneywait@gmail.com>
2019-04-24 14:50:50 -0400

    finished exercises with lists

---
+++ dictionaries/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Create a dictionary with key value pairs to
+                represent words (key) and its definition (value)
+            */
+            Dictionary<string, string> wordsAndDefinitions = new Dictionary<string, string>();
+
+            // Add several more words and their definitions
+            wordsAndDefinitions.Add("Awesome", "The feeling of students when they are learning C#");
+            wordsAndDefinitions.Add("Monday", "The worst day of the week");
+            wordsAndDefinitions.Add("Taco", "It can be anything, really");
+            wordsAndDefinitions.Add("brutiful", "The beautiful brutal process of learning to code. ");
+
+            /*
+                Use square bracket lookup to get the definition two
+                words and output them to the console
+            */
+
+            /*
+                Loop over dictionary to get the following output:
+                    The definition of [WORD] is [DEFINITION]
+                    The definition of [WORD] is [DEFINITION]
+                    The definition of [WORD] is [DEFINITION]
+            */
+            foreach (KeyValuePair<string, string> word in wordsAndDefinitions)
+            {
+            }
+        }
+    }
+}
--- lists/Program.cs
+++ lists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace lists
 {
@@ -6,7 +7,73 @@ namespace lists
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+
+            // Add() Jupiter and Saturn at the end of the list.
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+
+            // Create another List that contains that last two planet of our solar system.
+
+            List<string> otherPlanetList = new List<string>(){
+                "Uranus",
+                "Neptune"
+            };
+            // Combine the two lists by using AddRange().
+
+            planetList.AddRange(otherPlanetList);
+            // Use Insert() to add Earth, and Venus in the correct order.
+            planetList.Insert(3, "Earth");
+            planetList.Insert(2, "Venus");
+            // Use Add() again to add Pluto to the end of the list.
+            planetList.Add("Pluto");
+
+            // Now that all the planets are in the list, slice the list using GetRange() in order to extract the rocky planets into a new list called rockyPlanets. The rocky planets will remain in the original planets list.
+
+            List<string> rockyPlanets = new List<string>() { };
+            rockyPlanets.AddRange(planetList.GetRange(0, 4));
+            Console.WriteLine("List of the Rocky Planets:");
+            rockyPlanets.ForEach(planet => Console.WriteLine($" -{planet}"));
+
+            Console.WriteLine("List of all the Planets:");
+            planetList.ForEach(planet => Console.WriteLine($" -{planet}"));
+
+            // Being good amateur astronomers, we know that Pluto is now a dwarf planet, so use the Remove() method to eliminate it from the end of planetList.
+            planetList.Remove("Pluto");
+            Console.WriteLine("Planet List without Pluto:");
+            planetList.ForEach(planet => Console.WriteLine($" -{planet}"));
+
+
+            Random random = new Random();
+            List<int> numbers = new List<int> {
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+            };
+
+            Console.WriteLine($"length of numberList {numbers.Count}");
+
+            for (int i = 0; i < numbers.Count; i++)
+
+            {
+                // Determine if the current loop index is contained inside of the `numbers` list. Print a message to the console indicating whether the index is in the list.
+                if(numbers.Contains(i))
+                {
+                Console.WriteLine($"the number list contains {i}");
+                }
+                else{
+                    Console.WriteLine($"The number list does not contain {i}");
+                }
+
+            }
+
+            // End Main
         }
     }
 }
