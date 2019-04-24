5df172946a7a6e06254af617dfa1c1e4f2a97cfe
sydneywait <sydneywait@gmail.com>
2019-04-24 15:10:54 -0400

    finished second dictionary exercise

--- dictionaries/Program.cs
+++ dictionaries/Program.cs
@@ -24,6 +24,8 @@ namespace dictionaries
                 words and output them to the console
             */
 
+            Console.WriteLine(wordsAndDefinitions["Awesome"]);
+            Console.WriteLine(wordsAndDefinitions["brutiful"]);
             /*
                 Loop over dictionary to get the following output:
                     The definition of [WORD] is [DEFINITION]
@@ -32,7 +34,92 @@ namespace dictionaries
             */
             foreach (KeyValuePair<string, string> word in wordsAndDefinitions)
             {
+                Console.WriteLine($"The defintion of {word.Key} is {word.Value}");
+
             }
+
+            // Make a new list
+            List<Dictionary<string, string>> dictionaryOfWords = new List<Dictionary<string, string>>();
+
+            /*
+                You want to track the following about each word:
+                    word, definition, part of speech, example sentence
+
+                Example of one dictionary in the list:
+                {
+                    "word": "excited",
+                    "definition": "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness",
+                    "part of speech": "adjective",
+                    "example sentence": "I am excited to learn C#!"
+                }
+            */
+
+            // Create dictionary to represent a new word
+            Dictionary<string, string> excitedWord = new Dictionary<string, string>();
+
+            // Add each of the 4 bits of data about the word to the Dictionary
+            excitedWord.Add("word", "excited");
+            excitedWord.Add("definition", "having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness");
+            excitedWord.Add("part of speech", "adjective");
+            excitedWord.Add("example sentence", "I am excited to learn C#!");
+
+
+
+
+
+            // Add Dictionary to your `dictionaryOfWords` list
+            dictionaryOfWords.Add(excitedWord);
+
+
+            // create another Dictionary and add that to the list
+            Dictionary<string, string> brutifulWord = new Dictionary<string, string>();
+            brutifulWord.Add("word", "brutiful");
+            brutifulWord.Add("definition", "Characterized by being beautiful and brutal");
+            brutifulWord.Add("part of speech", "adjective");
+            brutifulWord.Add("example sentence", "learning C# is very brutiful!");
+
+            dictionaryOfWords.Add(brutifulWord);
+
+
+            /*
+                Iterate your list of dictionaries and output the data
+
+                Example output for one word in the list of dictionaries:
+                    word: excited
+                    definition: having, showing, or characterized by a heightened state of energy, enthusiasm, eagerness
+                    part of speech: adjective
+                    example sentence: I am excited to learn C#!
+            */
+
+            // Iterate the List of Dictionaries
+
+            dictionaryOfWords.ForEach(word =>
+            {
+                // Iterate the KeyValuePairs of the Dictionary
+                foreach (KeyValuePair<string, string> wordData in word)
+                {
+                    Console.WriteLine($"{wordData.Key}: {wordData.Value}");
+                }
+                Console.WriteLine("-----------------------------------");
+
+
+            });
+
+
+            Dictionary<string, List<string>> idioms = new Dictionary<string, List<string>>();
+            idioms.Add("Penny", new List<string> { "A", "penny", "for", "your", "thoughts" });
+            idioms.Add("Injury", new List<string> { "Add", "insult", "to", "injury" });
+            idioms.Add("Moon", new List<string> { "Once", "in", "a", "blue", "moon" });
+            idioms.Add("Grape", new List<string> { "I", "heard", "it", "through", "the", "grapevine" });
+            idioms.Add("Murder", new List<string> { "Kill", "two", "birds", "with", "one", "stone" });
+            idioms.Add("Limbs", new List<string> { "It", "costs", "an", "arm", "and", "a", "leg" });
+            idioms.Add("Grain", new List<string> { "Take", "what", "someone", "says", "with", "a", "grain", "of", "salt" });
+            idioms.Add("Fences", new List<string> { "I'm", "on", "the", "fence", "about", "it" });
+            idioms.Add("Sheep", new List<string> { "Pulled", "the", "wool", "over", "his", "eyes" });
+            idioms.Add("Lucifer", new List<string> { "Speak", "of", "the", "devil" });
+
+
+            // end of Main
         }
     }
 }
