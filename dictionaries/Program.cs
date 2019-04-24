ca337b15c853d08620393e4673dc5edaf884a6a5
sydneywait <sydneywait@gmail.com>
2019-04-24 15:46:46 -0400

    finished practice exercise restructuring javascript data to csharp data

--- dictionaries/Program.cs
+++ dictionaries/Program.cs
@@ -64,9 +64,6 @@ namespace dictionaries
             excitedWord.Add("example sentence", "I am excited to learn C#!");
 
 
-
-
-
             // Add Dictionary to your `dictionaryOfWords` list
             dictionaryOfWords.Add(excitedWord);
 
@@ -107,6 +104,7 @@ namespace dictionaries
 
 
             Dictionary<string, List<string>> idioms = new Dictionary<string, List<string>>();
+
             idioms.Add("Penny", new List<string> { "A", "penny", "for", "your", "thoughts" });
             idioms.Add("Injury", new List<string> { "Add", "insult", "to", "injury" });
             idioms.Add("Moon", new List<string> { "Once", "in", "a", "blue", "moon" });
@@ -117,6 +115,103 @@ namespace dictionaries
             idioms.Add("Fences", new List<string> { "I'm", "on", "the", "fence", "about", "it" });
             idioms.Add("Sheep", new List<string> { "Pulled", "the", "wool", "over", "his", "eyes" });
             idioms.Add("Lucifer", new List<string> { "Speak", "of", "the", "devil" });
+
+// Print the word and idiom phrase to the console
+foreach (KeyValuePair<string, List<string>> idiom in idioms){
+    string phrase ="";
+    foreach (string word in idiom.Value){
+        phrase = phrase + " " + word;
+    }
+    Console.WriteLine($"{idiom.Key}: {phrase}");
+}
+
+// Take the following JavaScript data structure that represents car sales and convert it to C# Lists and Dictionaries. Make sure that all of your variable names conform to the C# standard of Camel Case instead of Snake Case.
+Dictionary<string, string> vehicle = new Dictionary<string,string>();
+
+vehicle.Add("year", "2008");
+vehicle.Add("model", "Damfresh");
+vehicle.Add("make", "Biotraxquote");
+vehicle.Add("color", "Sky Magenta");
+
+Dictionary<string, string> salesId =new Dictionary<string,string>();
+
+salesId.Add("salesId","ecb1c841-1a43-4a7c-896e-712d2ec39c71");
+
+Dictionary<string,string> salesAgent=new Dictionary<string,string>();
+salesAgent.Add("mobile", "(896) 478-6975");
+salesAgent.Add("last_name","Botsford");
+salesAgent.Add("first_name","Shaina");
+salesAgent.Add("emails", "beatae_sonny@hotmail.com or shaina@aol.com");
+
+Dictionary<string,string> purchaseDate=new Dictionary<string,string>();
+purchaseDate.Add("purchase_date","2017-11-15");
+
+Dictionary<string,string> grossProfit = new Dictionary<string,string>();
+grossProfit.Add("gross_profit", "871.26");
+
+Dictionary<string,string> credit = new Dictionary<string, string>();
+
+credit.Add("credit_provider", "J.P.Morgan Chase & Co");
+credit.Add("account", "601109582720302");
+
+List<Dictionary<string,string>> Vehicles = new List<Dictionary<string,string>>(){
+vehicle, salesId, salesAgent, purchaseDate, grossProfit, credit
+
+};
+
+
+
+
+
+
+
+// "vehicles": [
+//     {
+//         "vehicle": {
+//             "year": 2008",
+//             "model": "Damfresh",
+//             "make": "Biotraxquote",
+//             "color": "sky magenta"
+//         },
+//         "sales_id": "ecb1c841-1a43-4a7c-896e-712d2ec39c71",
+//         "sales_agent": {
+//             "mobile": "(896) 478-6975",
+//             "last_name": "Botsford",
+//             "first_name": "Shaina",
+//             "emails": ["beatae_sonny@hotmail.com", "shaina@aol.com"]
+//         },
+//         "purchase_date": "2017-11-15",
+//         "gross_profit": 871.26,
+//         "credit": {
+//             "credit_provider": "J.P.Morgan Chase & Co",
+//             "account": "601109582720302"
+//         }
+//     },
+//     {
+//         "vehicle": {
+//             "year": 2010,
+//             "model": "Hotquadtrax",
+//             "make": "Transtintechno",
+//             "color": "robin egg blue"
+//         },
+//         "sales_id": "a2f80554-bd9d-4ea1-8229-01fd4cf220a8",
+//         "sales_agent": {
+//             "mobile": "562.300.2912",
+//             "last_name": "Davis",
+//             "first_name": "Gerardo",
+//             "emails": ["girl70@hotmail.com", "jova43@gmail.com"]
+//         },
+//         "purchase_date": "2017-04-28",
+//         "gross_profit": 156.02,
+//         "credit": {
+//             "credit_provider": "PNC Financial Services",
+//             "account": "34578280562836"
+//         }
+//     }
+// ]
+
+
+
 
 
             // end of Main
