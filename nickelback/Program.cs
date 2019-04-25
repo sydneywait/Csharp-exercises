8dd392e286f7c33e660c7e4d08885289d3445eb2
sydneywait <sydneywait@gmail.com>
2019-04-25 12:03:06 -0400

    fixed formatting

--- nickelback/Program.cs
+++ nickelback/Program.cs
@@ -22,13 +22,13 @@ namespace nickelback
             // HashSet<Dictionary<string, string>> allSongs = new HashSet<Dictionary<string, string>>();
             HashSet<Dictionary<string, string>> allSongs = new HashSet<Dictionary<string, string>>(){
 
-new Dictionary<string,string>(){{"Nickelback","How You Remind Me"}},
-new Dictionary<string,string>(){{"Nickelback","Burn it to the ground"}},
-new Dictionary<string,string>(){{"Post Malone","Wow"}},
-new Dictionary<string,string>(){{"Ariana Grande","Thank you next"}},
-new Dictionary<string,string>(){{"Lil Nas X","Old Town Road"}},
-new Dictionary<string,string>(){{"Halsey","High Hopes"}},
-new Dictionary<string,string>(){{"Drake","Nice for What"}}
+            new Dictionary<string,string>(){{"Nickelback","How You Remind Me"}},
+            new Dictionary<string,string>(){{"Nickelback","Burn it to the ground"}},
+            new Dictionary<string,string>(){{"Post Malone","Wow"}},
+            new Dictionary<string,string>(){{"Ariana Grande","Thank you next"}},
+            new Dictionary<string,string>(){{"Lil Nas X","Old Town Road"}},
+            new Dictionary<string,string>(){{"Halsey","High Hopes"}},
+            new Dictionary<string,string>(){{"Drake","Nice for What"}}
 
 };
 
