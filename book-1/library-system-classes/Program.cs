d4c66cdf2363ecdfadd085cfbde21ff5208819d5
sydneywait <sydneywait@gmail.com>
2019-04-26 16:45:16 -0400

    adding console functionality for librarians to choose options

--- book-1/library-system-classes/Program.cs
+++ book-1/library-system-classes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace librarySystem
 {
@@ -34,15 +35,103 @@ namespace librarySystem
             Huntington.printInventoryReport();
             Barboursville.printInventoryReport();
 
-            // Check out a book to a patron
+            // Check out a book to a patron (book, library, patron)
 
             Holly.checkOutBook(Grapes, Huntington, Isaac);
 
             Isaac.printLoanReport();
-            Grapes.dueDate=DateTime.Today.AddDays(-10);
+            Grapes.dueDate = DateTime.Today.AddDays(-10);
             Holly.returnBook(Grapes, Huntington, Isaac);
 
             Isaac.payOverdueFees(4);
+
+            // Make a list of all the books in the database
+            List<Book> allBooks = new List<Book>()
+            {Grapes, Harry, DQ, RJ};
+
+            // Make a list of all the patrons in the database
+            List<Patron> allPatrons = new List<Patron>() { Sydney, Isaac };
+
+            // Make a list of all the Libraries in the database
+            List<Library> allLibraries = new List<Library>() { Huntington, Barboursville };
+
+            // Ask the librarian to choose an option for performing tasks
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\t1 - Check Out Book");
+            Console.WriteLine("\t2 - Return Book");
+            Console.WriteLine("\t3 - Pay Overdue Fees");
+            Console.WriteLine("\t4 - Print Library Inventory");
+            Console.WriteLine("\t5 - Print Customer's Loans");
+            Console.Write("Your option? ");
+
+
+            // Use a switch statement to run the logic for each option
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Book bookParam = new Book("", "");
+
+                    Console.WriteLine($"Enter the name of the book:");
+                    string bookName = Console.ReadLine();
+                    foreach (Book book in allBooks)
+                    {
+                        if (book.title == bookName)
+                        {
+                            bookParam = book;
+                            Console.WriteLine($"found book {book.title}");
+                            break;
+                        }
+
+                    }
+
+                    Library libraryParam = new Library(" ", " ");
+                    Console.WriteLine($"Enter the name of the library");
+                    string libraryName = Console.ReadLine();
+
+                    foreach(Library library in allLibraries){
+
+                        if(library.name==libraryName){
+                            libraryParam=library;
+                            break;
+                        }
+                    }
+
+                    Patron patronParam = new Patron("", "", new Library("",""));
+                    Console.WriteLine($"Enter the name of the patron");
+                    string patronName = Console.ReadLine();
+
+
+                    foreach (Patron patron in allPatrons)
+                    {
+                        if (patron.firstName == patronName)
+                        {
+                            patronParam = patron;
+                            break;
+                        }
+                        else
+                        {
+
+                        }
+                    }
+
+
+                    Holly.checkOutBook(bookParam, libraryParam, patronParam);
+                    break;
+
+                case "2":
+                    Console.WriteLine($"");
+                    break;
+                case "3":
+                    Console.WriteLine($"");
+                    break;
+                case "4":
+                    Console.WriteLine($"");
+                    break;
+                case "5":
+                    Console.WriteLine($"");
+                    break;
+            }
+
 
         }
     }
