6042f49f4a0da42e7f4f91f9b258f46e712ed631
sydneywait <sydneywait@gmail.com>
2019-04-26 13:17:17 -0400

    moved file structure into book-1

--- book-1/student-exercises-classes/Student.cs
+++ book-1/student-exercises-classes/Student.cs
@@ -4,17 +4,11 @@ using System.Collections.Generic;
 namespace student_exercises_classes
 {
     class Student:Person
-
-
-
     {
         public Student(string firstNameParam, string lastNameParam, string slackParam):base(firstNameParam, lastNameParam, slackParam){
 
             exercises = new List<Exercise>();
         }
-
-
-
         public List<Exercise> exercises {get; set;}
 
     }
--- dictionaries/Dictionary.cs
+++
@@ -1,6 +0,0 @@
-namespace dictionaries
-{
-    internal class Dictionary<T>
-    {
-    }
-}
