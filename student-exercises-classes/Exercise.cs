01d48c6a2a9b01f18391420fe8ac806b19832858
sydneywait <sydneywait@gmail.com>
2019-04-26 12:39:14 -0400

    assigned students and instructors to cohorts, gave student assignments and made report

---
+++ student-exercises-classes/Exercise.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+namespace student_exercises_classes
+{
+    class Exercise
+
+    {
+
+        public Exercise(string nameParam, string langParam)
+        {
+            name = nameParam;
+            programLang = langParam;
+
+        }
+        public string name { get; set; }
+        public string programLang { get; set; }
+
+
+    }
+}
