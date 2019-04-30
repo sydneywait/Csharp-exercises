80a52cef8dd3e779a4749cd79e1308ee169b280e
sydneywait <sydneywait@gmail.com>
2019-04-30 12:17:39 -0400

    started working on LINQ exercises for creating reports

--- book-1/student-exercises/Program.cs
+++ book-1/student-exercises/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace student_exercises
 {
@@ -12,7 +13,7 @@ namespace student_exercises
             // Create 4, or more, exercises.
 
             Exercise urbanPlanner = new Exercise("Urban Planner", "C#");
-            Exercise overlyExcited = new Exercise("Overly Excited", "Javascript");
+            Exercise overlyExcited = new Exercise("Overly Excited", "JavaScript");
             Exercise customTypes = new Exercise("Custom Types", "C#");
             Exercise getCoffee = new Exercise("Get Coffee", "Java");
 
@@ -70,10 +71,21 @@ namespace student_exercises
 
             };
 
-            // Create a list of exercises.Add all of the exercise instances to it.
+            // Create a list of exercises. Add all of the exercise instances to it.
             List<Exercise> exercises = new List<Exercise>(){
               urbanPlanner, overlyExcited, customTypes, getCoffee
             };
+
+            // Create a list of instructors, Add all of the instructor instances to it.
+            List<Instructor> instructors = new List<Instructor>(){
+                Kim, Jordan, Josh, Bill, Steve
+            };
+
+            // Create a list of Cohorts, Add all of the cohort instances to it.
+            List<Cohort> cohorts = new List<Cohort>(){
+              cohort1, cohort2, cohort3
+            };
+
 
             // Generate a report that displays which students are working on which exercises.
 
@@ -97,6 +109,32 @@ namespace student_exercises
                 }
             }
 
+
+            // *********Part 2--Using LINQ***********
+            //For this exercise, you need to create 4 new List instances to Program.cs: one to contain students, one to contain exercises, one to contain instructors, and one to contain cohorts.
+            // After your code where you created all of your instances, add each one to the lists.
+
+
+            // List exercises for the JavaScript language by using the Where() LINQ method.
+
+            List<Exercise> languages = exercises.Where(l => l.programLang == "JavaScript").ToList();
+            Console.WriteLine("Exercises written in Javascript:");
+            languages.ForEach(l => Console.WriteLine($" {l.name}"));
+            Console.WriteLine("-------------------");
+
+            // List students in a particular cohort by using the Where() LINQ method.
+            var studentsInCohort = cohorts.Where(c => c.name == "Cohort 1").OfType<List<Student>>();
+
+
+
+
+
+            // List instructors in a particular cohort by using the Where() LINQ method.
+            // Sort the students by their last name.
+
+            // Display any students that aren't working on any exercises (Make sure one of your student instances don't have any exercises. Create a new student if you need to.)
+            // Which student is working on the most exercises? Make sure one of your students has more exercises than the others.
+            // How many students in each cohort?
 
 
             // End Main
